0b6688b0daba69bb87b7e16726d1d54474fa3dc6
mohamed zakaria <moddm766@gmail.com>
2026-07-08 16:48:01 +0300

    feat : add DB connection

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import { bootstrap } from "./src/app.controller.js";
+import { checkDBconnection } from "./src/DB/connection.js";
 
 const app = express();
 const PORT = process.env.PORT;
+checkDBconnection();
 
 bootstrap(app, express);
 
-app.listen(PORT, () => console.log(`Example app listening on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Example app listening on port http://localhost:${PORT}`),
+);
